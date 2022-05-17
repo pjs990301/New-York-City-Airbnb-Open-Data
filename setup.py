7b27e27381ff0395335cd65803e7ead3db9e91e9
Pio <p990301@gmail.com>
2022-05-17 20:00:33 +0900

    make

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="New York Airbnb Open Data",
+    name="NewYorkAirbnbOpenData",
     version="0.0.1",
     author="Pio",
     author_email="p990301@gmail.com",
     description="New York Airbnb Open Data package",
     url="https://https://github.com/pjs990301/New-York-City-Airbnb-Open-Data",
     python_requires=">=3.6",
-    packages=find_packages(where="New_York_Airbnb"),
+    packages=find_packages(),
     install_requires=['numpy>=1.21.6',
                       'scikit-learn>=1.0.2',
                       'seaborn>=0.11.2']
